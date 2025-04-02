97068d141bb0dff4d26c72179ec670a97e9786bc
petezahv4 <petezahiscool@gmail.com>
2025-04-02 16:43:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,75 @@
+
 import { createBareServer } from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join } from "node:path";
+import { hostname } from "node:os";
 
-const bareServer = createBareServer("/bare/");
-app.use(express.static(pubDir));
+const bare = createBareServer("/bare/");
+const app = express();
 
-app.get("/uv/config.js", (req, res) => {
-        res.sendFile(path.join(pubDir, "uv/config.js"));
-});
+// Load our publicPath first and prioritize it over 
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's config.js won't conflict with our config.js inside the publicPath directory.
+app.use("/szvy/", express.static(uvPath));
 
-app.use("/uv/", express.static(uvPath));
-app.use("/epoxy/", express.static(epoxyPath));
-app.use("/libcurl/", express.static(libcurlPath));
-app.use("/baremux/", express.static(baremuxPath));
-
+// Error for everything else
 app.use((req, res) => {
-        res.status(404).sendFile(path.join(pubDir, "404.html"));
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
 });
 
-server.on("request", async (req, res) => {
-  // Listen for request abort events on the underlying request object
-  req.on("aborted", () => {
-    console.warn("Underlying request aborted:", req.url);
-  });
-  try {
-    if (bareServer.shouldRoute(req)) {
-      bareServer.routeRequest(req, res);
-    } else {
-      app(req, res);
-    }
-  } catch (error) {
-    if (error.message && error.message.includes("aborted")) {
-      console.warn("Request aborted by client during processing:", error);
-      return;
-    }
-    console.error("Request error:", error);
-    res.statusCode = 500;
-    res.write(String(error));
-    res.end();
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
   }
 });
 
-server.listen(port, "0.0.0.0", () => {
-        const address = server.address();
-        console.log(startup_msg)
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 80;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
+});
